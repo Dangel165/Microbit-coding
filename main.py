191b7878bcea2519334128f86b7f691087951703
Dangel <sid753855@gmail.com>
2025-11-09 19:29:26 +0900

    Micron Autonomous Driving

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+robot.df_robot_maqueen.start()
+
 def on_forever():
-    basic.show_leds("""
-        # . . . .
-        . . # . #
-        # . . . #
-        . # . . .
-        . . . # .
-        """)
+    if robot.detect_line(RobotLineDetector.LEFT) and robot.detect_line(RobotLineDetector.RIGHT):
+        robot.motor_steer(0, 100)
+    elif robot.detect_line(RobotLineDetector.LEFT) and robot.detect_line(RobotLineDetector.RIGHT):
+        robot.motor_steer(-100, 50)
+    elif not (robot.detect_line(RobotLineDetector.LEFT)) and robot.detect_line(RobotLineDetector.RIGHT):
+        robot.motor_steer(100, 50)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showLeds(`
-        # . . . .
-        . . # . #
-        # . . . #
-        . # . . .
-        . . . # .
-        `)
+robot.dfRobotMaqueen.start()
+basic.forever(function () {
+    if (robot.detectLine(RobotLineDetector.Left) && robot.detectLine(RobotLineDetector.Right)) {
+        robot.motorSteer(0, 70)
+    } else if (robot.detectLine(RobotLineDetector.Left) && !(robot.detectLine(RobotLineDetector.Right))) {
+        robot.motorSteer(-100, 50)
+    } else if (!(robot.detectLine(RobotLineDetector.Left)) && robot.detectLine(RobotLineDetector.Right)) {
+        robot.motorSteer(100, 50)
+    }
 })
